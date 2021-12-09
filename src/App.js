c636674b10e01f617a849dcdaae0b79c8d27c6e4
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-12-09 21:40:22 +0200

    Refactor the file structure inline with review requirements

--- src/App.js
+++ src/App.js
@@ -5,35 +5,8 @@ import {
 } from 'react-router-dom';
 import Header from './components/Nav';
 import Book from './components/Books';
-import NewBook from './components/NewBook';
+import bookList from './components/BookList';
 import Categories from './components/Categories';
-
-const books = [
-  {
-    id: 1,
-    category: 'Action',
-    name: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    percentage: '64%',
-    chapter: 'Chapter 17',
-  },
-  {
-    id: 2,
-    category: 'Science Fiction',
-    name: 'Dune',
-    author: 'Frank Herbert',
-    percentage: '8%',
-    chapter: 'Chapter 3: "A Lesson Learned"',
-  },
-  {
-    id: 3,
-    category: 'Economy',
-    name: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-    percentage: '0%',
-    chapter: 'Introduction',
-  },
-];
 
 const App = () => {
   const bookList = books.map((books) => <Book key={books.id} info={books} />);
@@ -43,7 +16,7 @@ const App = () => {
       <Switch>
         <Route exact path="/">
           { bookList }
-          <NewBook />
+          <bookList />
         </Route>
         <Route path="/categories">
           <Categories />
