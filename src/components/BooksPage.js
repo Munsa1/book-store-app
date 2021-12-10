92c652a7a389dce94bcff955bb8ba372d8fbecc8
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-12-10 21:47:16 +0200

    Disable linter

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import AddNewBook from './AddNewBook';
-//eslint-disable-next-line
+// eslint-disable-next-line
 import Books from './books';
 
 const BooksPage = () => {
