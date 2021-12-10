844e6b97d573e824c446be2f008d7c5c8ac9f799
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-12-10 13:58:34 +0200

    Update bookpage.js and readmefile

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -6,9 +6,9 @@ import Books from './Books';
 const BooksPage = () => {
   const bookList = [
     {
-      booktitle: ' The Great Gatsby',
-      author: ' F. Scott Fitzgerald',
-      category: ' Fiction',
+      booktitle: ' 16 Laws of Success',
+      author: ' Napoleon Hill',
+      category: ' Motivational',
       id: uuidv4(),
     },
   ];
