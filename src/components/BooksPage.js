32077a67ea52ca9a429d1a5ddb09d30bfc339882
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-12-10 21:42:42 +0200

    Add correct import

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import AddNewBook from './AddNewBook';
-import Books from './Books';
+import Books from './books';
 
 const BooksPage = () => {
   const bookList = useSelector((state) => state.booksReducer);
