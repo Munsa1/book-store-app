1ab41ae662eb71a45b1a1d4b02d68a8f76ea7e28
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-12-11 03:11:43 +0200

    Fix linters

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 // eslint-disable-next-line
-import booksReducer from './Books/books';
+import booksReducer from '../redux/Books/books';
 
 const reducer = combineReducers({
   booksReducer,
